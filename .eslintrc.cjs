ad3fa5197599398a4f2806d090f7b1696e59a6dd
Ma He <qmahe@live.com>
2023-11-24 00:39:58 +0800

    update linter rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,25 +8,18 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "react", "prefer-arrow-functions"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
-    ],
-    "react/function-component-definition": [
-      "error",
-      {
-        namedComponents: "function-declaration",
-        unnamedComponents: "function-expression",
-      },
     ],
     "no-console": ["error", { allow: ["warn", "error"] }],
     "no-alert": "error",
     "no-debugger": "error",
     "no-unused-vars": "error",
     indent: ["error", 2],
-    quotes: ["error", "single"],
+    quotes: ["error", "double"],
     "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/explicit-function-return-type": "error",
@@ -36,5 +29,50 @@ module.exports = {
     "no-console": ["warn", { allow: ["warn", "error"] }], // Allow console.warn and console.error
     "no-alert": "error", // Disallow the use of alert, confirm, and prompt
     "no-debugger": "error", // Disallow the use of debugger
+    "react/prefer-stateless-function": "error",
+    "react/button-has-type": "error",
+    "react/no-unused-prop-types": "error",
+    "react/jsx-pascal-case": "error",
+    "react/jsx-no-script-url": "error",
+    "react/no-children-prop": "error",
+    "react/no-danger": "error",
+    "react/no-danger-with-children": "error",
+    "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+    "react/jsx-fragments": "error",
+    "react/destructuring-assignment": [
+      "error",
+      "always",
+      { destructureInSignature: "always" },
+    ],
+    "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+    "react/jsx-max-depth": ["error", { max: 5 }],
+    "react/function-component-definition": [
+      "error",
+      { "function-declaration": "unnamedComponents" },
+    ],
+    "react/jsx-key": [
+      "error",
+      {
+        checkFragmentShorthand: true,
+        checkKeyMustBeforeSpread: true,
+        warnOnDuplicates: true,
+      },
+    ],
+    "react/jsx-no-useless-fragment": "warn",
+    "react/jsx-curly-brace-presence": "warn",
+    "react/no-typos": "warn",
+    "react/display-name": "warn",
+    "react/self-closing-comp": "warn",
+    "react/jsx-sort-props": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/prop-types": "off",
+    "arrow-body-style": "warn",
+    "prefer-arrow-callback": [
+      "warn",
+      {
+        allowNamedFunctions: true,
+      },
+    ],
   },
 };
