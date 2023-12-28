8252c2e235f542c8d92b7a4fb71f256ffeb30e14
Ma He <qmahe@live.com>
2023-12-29 02:37:39 +0800

    implement debouncer hook

---
+++ src/common/hooks/useDebouncer.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+type UseDebouncer = number | string;
+
+export default function useDebouncer(value: number | string, delay: number): UseDebouncer {
+    const [debouncedValue, setDebouncedValue] = useState<number | string>(value);
+
+    useEffect(() => {
+        let timeoutId: number;
+
+        const debounceFunction = (): void => {
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                setDebouncedValue(value);
+            }, delay);
+        };
+
+        debounceFunction();
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
