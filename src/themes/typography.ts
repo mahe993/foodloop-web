7400e9a2be444d3a827085aaa507f9b569554cbe
chae-seong <chaeseong123@hotmail.com>
2023-12-31 03:16:00 +0800

    change default font family to sans-serif and add new image assets

--- src/themes/typography.ts
+++ src/themes/typography.ts
@@ -1,29 +1,29 @@
 export const defaultText = {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: 'sans-serif',
     fontWeight: 400,
     fontSize: '1rem',
 };
 
 export const defaultBoldText = {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: 'sans-serif',
     fontWeight: 'bold',
     fontSize: '1rem',
 };
 
 export const smallLightText = {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: 'sans-serif',
     fontWeight: 300,
     fontSize: '0.75rem',
 };
 
 export const smallBoldText = {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: 'sans-serif',
     fontWeight: 'bold',
     fontSize: '0.75rem',
 };
 
 export const xSmallLightText = {
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: 'sans-serif',
     fontWeight: 300,
     fontSize: '0.6rem',
 };
