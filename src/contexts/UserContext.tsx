c425db7ab555b2ec412b57eadb2329613feac88e
Ma He <qmahe@live.com>
2024-03-27 14:12:39 +0800

    set default user

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -11,17 +11,20 @@ const UserCtx = createContext<UserContext>({ id: 0, name: '' });
 export default function UserContextProvider({ children }: { children: ReactNode }): JSX.Element {
     const [user, setUser] = useState<User>({ id: 0, name: '' });
 
+    const getAndSetUser = async (id: number): Promise<void> => {
+        try {
+            const usr = await getUser(newAbortSignal(), { userID: id });
+            setUser(usr);
+        } catch (e) {
+            setUser({ id: 1, name: 'Error connecting ...' });
+            console.log(e);
+        }
+    };
+
     useEffect(() => {
         // fetch user data
         const { id } = getContributors()[0];
-        try {
-            (async (): Promise<void> => {
-                const usr = await getUser(newAbortSignal(), { userID: id });
-                setUser(usr);
-            })();
-        } catch (e) {
-            console.log(e);
-        }
+        getAndSetUser(id);
     }, []);
 
     return <UserCtx.Provider value={user}>{children}</UserCtx.Provider>;
